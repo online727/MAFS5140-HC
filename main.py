ba267fb0c8fdbc45e13a59fa96af7f15933a549d
online <zhhohoh27@outlook.com>
2026-03-14 23:57:30 +0800

    Initialize repo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from strategy import Strategy
 
 def main():
     # 1. Define the path to the dataset
-    data_path = "data_downloader/test.parquet" 
+    data_path = "data/mini1/validation.parquet" 
     
     try:
         # 2. Initialize components
